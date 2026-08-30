a5b3eabd7d7be7c83698e178d11a7b93509fe36c
OrbitGlitch9 <322343071+OrbitGlitch9@users.noreply.github.com>
2026-08-30 17:29:45 -0400

    Initial project setup

---
+++ app/src/main/java/com/orbitglitch/fullscreenbrowsers/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.orbitglitch.fullscreenbrowsers
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.orbitglitch.fullscreenbrowsers.ui.theme.FullScreenBrowsersTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FullScreenBrowsersTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FullScreenBrowsersTheme {
+        Greeting("Android")
+    }
+}
